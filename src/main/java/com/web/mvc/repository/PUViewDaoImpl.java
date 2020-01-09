48c7fad7c8ba78368d289561b9719192a76eb9cd
study <study@MB-207-15>
2020-01-09 21:47:46 +0800

    隨便打

---
+++ src/main/java/com/web/mvc/repository/PUViewDaoImpl.java
@@ -0,0 +1,31 @@
+package com.web.mvc.repository;
+
+import com.web.mvc.entity.PUView;
+import com.web.mvc.repository.spec.PUViewDao;
+import com.web.mvc.repository.spec.PUViewDao;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository(value = "puViewDao")
+public class PUViewDaoImpl implements PUViewDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    @Override
+    public List<PUView> queryPUView() {
+        String sql = "SELECT * FROM PU_VIEW";
+        List<PUView> list = jdbcTemplate.query(sql, RM.puViewMapper);
+        return list;
+    }
+
+    @Override
+    public List<PUView> queryPUViewByProductCodeName(String productCodeName) {
+        String sql = "SELECT * FROM PU_VIEW WHERE PRODUCT_CODE_NAME = ?";
+        List<PUView> list = jdbcTemplate.query(sql, new Object[]{productCodeName}, RM.puViewMapper);
+        return list;
+    }
+    
+}
